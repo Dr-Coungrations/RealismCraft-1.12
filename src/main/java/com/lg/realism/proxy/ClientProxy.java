af8d3631b358a5022b06a41d50e4f841cb88d907
Maxim Cvetkov <maximpixel@yandex.ru>
2017-11-11 13:37:45 +0200

    Add files via upload

--- src/main/java/com/lg/realism/proxy/ClientProxy.java
+++ src/main/java/com/lg/realism/proxy/ClientProxy.java
@@ -5,28 +5,39 @@ import net.minecraft.client.renderer.color.BlockColors;
 import net.minecraft.client.renderer.color.IBlockColor;
 import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.client.renderer.color.ItemColors;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ColorizerGrass;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.biome.BiomeColorHelper;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import org.lwjgl.input.Keyboard;
+
+import com.lg.realism.Realism;
 import com.lg.realism.RegBlocks;
 import com.lg.realism.RegItems;
 import com.lg.realism.Sounds.SoundsRegister;
+import com.lg.realism.event.EventToolMode;
 import com.lg.realism.event.RenderTextEvent;
 
 public class ClientProxy extends CommonProxy {
-
+	
+	public static String KEY_CATEGORY = "key.categories." + Realism.MODID;
+	
+	public static EventToolMode.EnumToolMode currentToolMode = EventToolMode.EnumToolMode.FULL;
+	
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
 		super.preInit(event);
-
+		RegItems.preRegisterRender();
 	}
 
 	@Override
@@ -35,10 +46,13 @@ public class ClientProxy extends CommonProxy {
 		RegBlocks.registerRender();
 		RegItems.registerRender();
 		SoundsRegister.register();
-		 ClientRegistry.registerKeyBinding(RenderTextEvent.pressKeyOne); 
-		 ClientRegistry.registerKeyBinding(RenderTextEvent.pressKeyTwo); 
-		 ClientRegistry.registerKeyBinding(RenderTextEvent.pressKeyCharge); 
-		 ClientRegistry.registerKeyBinding(RenderTextEvent.pressKeyChargeM); 
+		
+		ClientRegistry.registerKeyBinding(RenderTextEvent.pressKeyOne); 
+		ClientRegistry.registerKeyBinding(RenderTextEvent.pressKeyTwo); 
+		ClientRegistry.registerKeyBinding(RenderTextEvent.pressKeyCharge); 
+		ClientRegistry.registerKeyBinding(RenderTextEvent.pressKeyChargeM);
+		
+		ClientRegistry.registerKeyBinding(EventToolMode.KEY_TOOL_MODE);
 	}
 
 	@Override

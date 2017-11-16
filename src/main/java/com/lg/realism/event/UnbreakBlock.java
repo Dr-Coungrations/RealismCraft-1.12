36ee474c6f8dcb123459ec6536f4820d47b17bc7
Maxim Cvetkov <maximpixel@yandex.ru>
2017-11-16 21:38:17 +0200

    Add files via upload

--- src/main/java/com/lg/realism/event/UnbreakBlock.java
+++ src/main/java/com/lg/realism/event/UnbreakBlock.java
@@ -1,5 +1,7 @@
 package com.lg.realism.event;
 
+import com.lg.realism.Items.ItemRealPickaxe;
+
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemPickaxe;
@@ -11,20 +13,20 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class UnbreakBlock {
 	
 	@SubscribeEvent
-    public void unbreakeableWood(PlayerEvent.BreakSpeed event) {
-        if (event.getState().getBlock() == Blocks.LOG) {
-            if (canBlockItemAxe(event.getEntityPlayer().getHeldItem(EnumHand.MAIN_HAND))) event.setCanceled(true);
-        } else if (event.getState().getBlock() == Blocks.LOG2) {
-            if (canBlockItemAxe(event.getEntityPlayer().getHeldItem(EnumHand.MAIN_HAND))) event.setCanceled(true);
-        }
-        else if (event.getState().getBlock() == Blocks.STONE || event.getState().getBlock() == Blocks.COBBLESTONE  || event.getState().getBlock() == Blocks.COBBLESTONE_WALL) {
-            if (canBlockItemPickaxe(event.getEntityPlayer().getHeldItem(EnumHand.MAIN_HAND))) event.setCanceled(true);
-        }
-    }
-	  private static boolean canBlockItemPickaxe(ItemStack itemStack) {
-	        return itemStack == null || !(itemStack.getItem() instanceof ItemPickaxe);
-	    }
-    private static boolean canBlockItemAxe(ItemStack itemStack) {
-        return itemStack == null || !(itemStack.getItem() instanceof ItemAxe);
-    }
+	public void unbreakeableWood(PlayerEvent.BreakSpeed event) {
+		if (event.getState().getBlock() == Blocks.LOG) {
+			if (canBlockItemAxe(event.getEntityPlayer().getHeldItem(EnumHand.MAIN_HAND))) event.setCanceled(true);
+		} else if (event.getState().getBlock() == Blocks.LOG2) {
+			if (canBlockItemAxe(event.getEntityPlayer().getHeldItem(EnumHand.MAIN_HAND))) event.setCanceled(true);
+		}
+		else if (event.getState().getBlock() == Blocks.STONE || event.getState().getBlock() == Blocks.COBBLESTONE  || event.getState().getBlock() == Blocks.COBBLESTONE_WALL) {
+			if (canBlockItemPickaxe(event.getEntityPlayer().getHeldItem(EnumHand.MAIN_HAND))) event.setCanceled(true);
+		}
+	}
+	private static boolean canBlockItemPickaxe(ItemStack itemStack) {
+		return itemStack == null || !(itemStack.getItem() instanceof ItemPickaxe || itemStack.getItem() instanceof ItemRealPickaxe);
+	}
+	private static boolean canBlockItemAxe(ItemStack itemStack) {
+		return itemStack == null || !(itemStack.getItem() instanceof ItemAxe || itemStack.getItem() instanceof ItemRealPickaxe);
+	}
 }
